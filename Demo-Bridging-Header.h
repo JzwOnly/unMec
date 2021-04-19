16d5efd059e02b0ed9b303440c84483e829e769d
JzwOnly <836579250@qq.com>
2021-04-19 16:01:02 +0800

    change bridging-header

--- Demo-Bridging-Header.h
+++ Demo-Bridging-Header.h
@@ -1,4 +1,6 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
+#ifndef Demo_BridgingHeader_h
+#define Demo_BridgingHeader_h
+
 #import "M3U8SegmentInfo+LocalPath.h"
+
+#endif /* Demo_BridgingHeader_h */
