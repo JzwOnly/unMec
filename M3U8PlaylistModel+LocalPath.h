b09caa77463dcc982e5f222c8b176c740f1537c3
JzwOnly <836579250@qq.com>
2021-04-20 09:20:52 +0800

    Update M3U8PlaylistModel+LocalPath.h

--- M3U8PlaylistModel+LocalPath.h
+++ M3U8PlaylistModel+LocalPath.h
@@ -1,3 +1,11 @@
+//
+//  M3U8PlaylistModel+_LocalPath.h
+//  Demo
+//
+//  Created by admin on 2021/4/20.
+//  Copyright © 2021 jzw. All rights reserved.
+//
+
 #import <M3U8Kit/M3U8Parser.h>
 
 @interface M3U8PlaylistModel (LocalPath)
