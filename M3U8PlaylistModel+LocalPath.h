0e7e018c945a334337a2b6b5b85f9e223866e647
JzwOnly <836579250@qq.com>
2021-04-20 12:53:29 +0800

    Update M3U8PlaylistModel+LocalPath.h

--- M3U8PlaylistModel+LocalPath.h
+++ M3U8PlaylistModel+LocalPath.h
@@ -9,5 +9,5 @@
 #import <M3U8Kit/M3U8Parser.h>
 
 @interface M3U8PlaylistModel (LocalPath)
-- (void)savePlaylistsToPath:(NSString *)path keymap:(NSDictionary *)keymap error:(NSError **)error;
+- (void)savePlaylistsToPath:(NSString *)path rewrite:(BOOL)rewrite keymap:(NSDictionary *)keymap error:(NSError **)error;
 @end
