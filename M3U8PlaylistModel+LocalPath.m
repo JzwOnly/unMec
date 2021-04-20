8e9dfb42875c3714db3101e0ef2ae9ded5f76ee2
JzwOnly <836579250@qq.com>
2021-04-20 12:52:11 +0800

    Update M3U8PlaylistModel+LocalPath.m

--- M3U8PlaylistModel+LocalPath.m
+++ M3U8PlaylistModel+LocalPath.m
@@ -11,42 +11,50 @@
 #import "M3U8PlaylistModel+LocalPath.h"
 
 @implementation M3U8PlaylistModel (LocalPath)
-- (void)savePlaylistsToPath:(NSString *)path keymap:(NSDictionary *)keymap error:(NSError **)error {
-    if ([[NSFileManager defaultManager] fileExistsAtPath:path]) {
-        if (NO == [[NSFileManager defaultManager] removeItemAtPath:path error:error]) {
+- (void)savePlaylistsToPath:(NSString *)path rewrite:(BOOL)rewrite keymap:(NSDictionary *)keymap error:(NSError **)error {
+    // 判断文件夹是否存在
+    if (NO == [[NSFileManager defaultManager] fileExistsAtPath:path]) {
+        if (NO == [[NSFileManager defaultManager] createDirectoryAtPath:path withIntermediateDirectories:YES attributes:nil error:error]) {
             return;
         }
     }
-    if (NO == [[NSFileManager defaultManager] createDirectoryAtPath:path withIntermediateDirectories:YES attributes:nil error:error]) {
-        return;
-    }
 
     if (self.masterPlaylist) {
-
         // master playlist
         NSString *masterContext = self.masterPlaylist.m3u8PlainString;
         for (int i = 0; i < self.masterPlaylist.xStreamList.count; i ++) {
             M3U8ExtXStreamInf *xsinf = [self.masterPlaylist.xStreamList xStreamInfAtIndex:i];
-            NSString *name = [NSString stringWithFormat:@"%@%d.m3u8", PREFIX_MAIN_MEDIA_PLAYLIST, i];
+            NSString *name = [NSString stringWithFormat:@"../%@%d.m3u8", PREFIX_MAIN_MEDIA_PLAYLIST, i];
             masterContext = [masterContext stringByReplacingOccurrencesOfString:xsinf.URI.absoluteString withString:name];
         }
         NSString *mPath = [path stringByAppendingPathComponent:self.indexPlaylistName];
-        BOOL success = [masterContext writeToFile:mPath atomically:YES encoding:NSUTF8StringEncoding error:error];
-        if (NO == success) {
-            NSLog(@"M3U8Kit Error: failed to save master playlist to file. error: %@", error?*error:@"null");
-            return;
+        BOOL isWrite = YES;
+        if ([[NSFileManager defaultManager] fileExistsAtPath:mPath]) {
+            if (rewrite) {
+                if (NO == [[NSFileManager defaultManager] removeItemAtPath:mPath error:error]) {
+                    return;
+                }
+            } else {
+                isWrite = NO;
+            }
         }
-
+        if (isWrite) {
+            BOOL success = [masterContext writeToFile:mPath atomically:YES encoding:NSUTF8StringEncoding error:error];
+            if (NO == success) {
+                NSLog(@"M3U8Kit Error: failed to save master playlist to file. error: %@", error?*error:@"null");
+                return;
+            }
+        }
         // main media playlist
-        [self saveMediaPlaylist:self.mainMediaPl toPath:path keymap:keymap error:error];
-        [self saveMediaPlaylist:self.audioPl toPath:path keymap:keymap error:error];
+        [self saveMediaPlaylist:self.mainMediaPl toPath:path rewrite:rewrite keymap:keymap error:error];
+        [self saveMediaPlaylist:self.audioPl toPath:path rewrite:rewrite keymap:keymap error:error];
 
     } else {
-        [self saveMediaPlaylist:self.mainMediaPl toPath:path keymap:keymap error:error];
+        [self saveMediaPlaylist:self.mainMediaPl toPath:path rewrite:rewrite keymap:keymap error:error];
     }
 }
 
-- (void)saveMediaPlaylist:(M3U8MediaPlaylist *)playlist toPath:(NSString *)path keymap:(NSDictionary *)keymap error:(NSError **)error {
+- (void)saveMediaPlaylist:(M3U8MediaPlaylist *)playlist toPath:(NSString *)path rewrite:(BOOL)rewrite keymap:(NSDictionary *)keymap error:(NSError **)error {
     if (nil == playlist) {
         return;
     }
@@ -69,12 +77,24 @@
         mainMediaPlContext = [mainMediaPlContext stringByReplacingOccurrencesOfString:key withString:value];
     }
     NSString *mainMediaPlPath = [path stringByAppendingPathComponent:playlist.name];
-    BOOL success = [mainMediaPlContext writeToFile:mainMediaPlPath atomically:YES encoding:NSUTF8StringEncoding error:error];
-    if (NO == success) {
-        if (NULL != error) {
-            NSLog(@"M3U8Kit Error: failed to save mian media playlist to file. error: %@", *error);
+    BOOL isWrite = YES;
+    if ([[NSFileManager defaultManager] fileExistsAtPath:mainMediaPlPath]) {
+        if (rewrite) {
+            if (NO == [[NSFileManager defaultManager] removeItemAtPath:mainMediaPlPath error:error]) {
+                return;
+            }
+        } else {
+            isWrite = NO;
         }
-        return;
+    }
+    if (isWrite) {
+        BOOL success = [mainMediaPlContext writeToFile:mainMediaPlPath atomically:YES encoding:NSUTF8StringEncoding error:error];
+        if (NO == success) {
+            if (NULL != error) {
+                NSLog(@"M3U8Kit Error: failed to save mian media playlist to file. error: %@", *error);
+            }
+            return;
+        }
     }
 }
 @end
