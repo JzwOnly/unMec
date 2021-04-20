c8153a446a2ae141eb2acdfe65d8954fcef3d2d3
JzwOnly <836579250@qq.com>
2021-04-20 09:15:57 +0800

    Update M3U8SegmentInfo+LocalPath.h

--- M3U8SegmentInfo+LocalPath.h
+++ M3U8SegmentInfo+LocalPath.h
@@ -1,5 +1,5 @@
 #import <M3U8Kit/M3U8Parser.h>
 
-@interface M3U8SegmentInfo (LocalPath)
+@interface M3U8PlaylistModel (LocalPath)
 - (void)savePlaylistsToPath:(NSString *)path keymap:(NSDictionary *)keymap error:(NSError **)error;
 @end
