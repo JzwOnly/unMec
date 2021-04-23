f99f4b44634caa426e7eafc164e044f9c39ea75b
JzwOnly <836579250@qq.com>
2021-04-23 15:40:20 +0800

    Create AppDelegate+Config.h

---
+++ AppDelegate+Config.h
@@ -0,0 +1,9 @@
+#import "AppDelegate.h"
+@interface AppDelegate (Config)
+- (void)doraemonKitConfig;
+- (void)qmuiConfig;
+- (void)registerNavgationRouter;
+- (void)registerSchemaRouter;
+- (void)configDataBase;
+- (void)registerNSUserDefaults;
+@end
