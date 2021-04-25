4989c3db56e0ccbcfe34900a84cb1f607b56539c
JzwOnly <836579250@qq.com>
2021-04-25 11:40:30 +0800

    Update Mole-Bridging-Header.h

--- Mole-Bridging-Header.h
+++ Mole-Bridging-Header.h
@@ -1,7 +1,11 @@
-#import <UIKit/UIKit.h>
-#import <Foundation/Foundation.h>
-
-// QMUI
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
 #import "QDCommonUI.h"
 #import "QDUIHelper.h"
 #import "QDThemeManager.h"
+#import "QMUIConfigurationTemplateDark.h"
+#import "QDNavigationController.h"
+#import "QDTabBarViewController.h"
+#import "QDCommonViewController.h"
+#import "QDCommonTableViewController.h"
