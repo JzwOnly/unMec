e56fa9c39bb2abb4ad651069efb0d56a5cef33ad
JzwOnly <836579250@qq.com>
2022-04-21 11:14:35 +0800

    Create JZTableViewModel.h

---
+++ JZTableViewModel.h
@@ -0,0 +1,19 @@
+//
+//  JZTableViewModel.h
+//  CMEPlayer
+//
+//  Created by admin on 2021/1/8.
+//  Copyright © 2021 admin. All rights reserved.
+//
+
+#import "JZViewModel.h"
+#import "EmptyModel.h"
+@interface JZTableViewModel : JZViewModel
+@property (nonatomic, readwrite , strong) EmptyModel * emptyModel;
+/// The data source of table view. 这里不能用NSMutableArray，因为NSMutableArray不支持KVO，不能被RACObserve
+@property (nonatomic, readwrite, copy) NSArray *dataSource;
+
+/// 选中命令 eg:  didSelectRowAtIndexPath:
+@property (nonatomic, readwrite, strong) RACCommand *didSelectCommand;
+@end
+
