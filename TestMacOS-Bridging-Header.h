7cbbd7a1e6afaf35c99b7fa562cd7449002c950b
JzwOnly <836579250@qq.com>
2022-06-25 15:58:23 +0800

    Update TestMacOS-Bridging-Header.h

--- TestMacOS-Bridging-Header.h
+++ TestMacOS-Bridging-Header.h
@@ -2,5 +2,5 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "JZButton.h"
-#import "JZLabel.h"
+#import "NNButton.h"
+#import "NNLabel.h"
